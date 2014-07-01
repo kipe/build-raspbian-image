3ecdebaa4f6080a77c4a663b8280ca53fc84e8ce
Simon Eisenmann <simon@struktur.de>
2014-07-01 13:01:54 +0200

    Generate keys from /dev/random. Create own certificate and key rather than using snake-oil one. Auto create keys for sessionSecret, encryptionSecret and serverToken on first boot.

--- delivery/task-spreed-webrtc.sh
+++ delivery/task-spreed-webrtc.sh
@@ -17,28 +17,51 @@ wget http://packages.struktur.de/spreed-webrtc/raspbian/struktur-debian-package-
 apt-get update
 apt-get install spreed-webrtc ssl-cert
 
+# Create SSL certificate configuration.
+mkdir -p /etc/spreed
+echo "
+#
+# SSLeay configuration file for Spreed.
+#
+
+RANDFILE                = /dev/random
+
+[ req ]
+default_bits            = 2048
+default_md              = sha256
+default_keyfile         = privkey.pem
+distinguished_name      = req_distinguished_name
+prompt                  = no
+policy                  = policy_anything
+
+[ req_distinguished_name ]
+commonName              = raspberrypi
+" > /etc/spreed/ssleay.cnf
+
 # Add user to ssl-cert group.
 gpasswd -a spreed-webrtc ssl-cert
 
 # Add first boot actions.
 echo "
+# Generate SSL key and self signed certificate.
+openssl req -new -x509 -nodes -keyout /etc/ssl/private/spreed-webrtc.key -out /etc/ssl/certs/spreed-webrtc.pem -days 3650 -config /etc/spreed/ssleay.cnf
+chmod 644 /etc/ssl/certs/spreed-webrtc.pem
+chmod 640 /etc/ssl/private/spreed-webrtc.key
+chown root:ssl-cert /etc/ssl/private/spreed-webrtc.key
 
-# Generate new ssl certificate.
-make-ssl-cert generate-default-snakeoil --force-overwrite
-
-# Use random sessionSecret.
-sed -i 's|sessionSecret = .*|sessionSecret = `openssl rand -hex 64`|' /etc/spreed/webrtc.conf
+# Generate random keys for Spreed WebRTC.
+sed -i \"s|sessionSecret = .*|sessionSecret = \$(xxd -ps -l 32 -c 32 /dev/random)|\" /etc/spreed/webrtc.conf
+sed -i \"s|encryptionSecret = .*|encryptionSecret = \$(xxd -ps -l 16 -c 16 /dev/random)|\" /etc/spreed/webrtc.conf
+sed -i \"s|;serverToken = .*|serverToken = \$(xxd -ps -l 16 -c 16 /dev/random)|\" /etc/spreed/webrtc.conf
 
 # Restart service.
 invoke-rc.d spreed-webrtc restart
-
 " >> /root/firstboot.sh
 
 # Enable SSL listener.
 sed -i 's|;listen = 127.0.0.1:8443|listen = 0.0.0.0:8443|' /etc/spreed/webrtc.conf
-sed -i 's|;certificate = .*|certificate = /etc/ssl/certs/ssl-cert-snakeoil.pem|' /etc/spreed/webrtc.conf
-sed -i 's|;key = .*|key = /etc/ssl/private/ssl-cert-snakeoil.key|' /etc/spreed/webrtc.conf
+sed -i 's|;certificate = .*|certificate = /etc/ssl/certs/spreed-webrtc.pem|' /etc/spreed/webrtc.conf
+sed -i 's|;key = .*|key = /etc/ssl/private/spreed-webrtc.key|' /etc/spreed/webrtc.conf
 sed -i 's|;stunURIs = .*|stunURIs = stun:stun.spreed.me:443|' /etc/spreed/webrtc.conf
-
 
 echo "done."
